4b07ac96f0c77f99d9ef0d50db84c912072a51ee
Joshua Singh <joshua.singh@ons.gov.uk>
2023-07-31 13:13:28 +0100

    Removed unused function and struct

--- handlers/errors.go
+++ handlers/errors.go
@@ -1,21 +1,8 @@
 package handlers
 
 import (
-	"errors"
 	"net/http"
 )
-
-// validationErr is an error which occurred while validating user input,
-// e.g. field cannot be less than x characters.
-type validationErr struct {
-	error
-}
-
-// isValidationErr checks to see if any error in the chain is a validationErr.
-func isValidationErr(err error) bool {
-	var vErr *validationErr
-	return errors.As(err, &vErr)
-}
 
 // clientErr is an error which occurred while validating client input,
 // e.g. request is missing a hidden form field.
