938440b46c11ee12518e3391832eae849ec90382
Joshua Singh <joshua.singh@ons.gov.uk>
2023-08-04 10:00:54 +0100

    Updated commit reference

--- config/config.go
+++ config/config.go
@@ -30,7 +30,7 @@ func Get() (*Config, error) {
 	if config.Debug {
 		config.PatternLibraryAssetsPath = "http://localhost:9002/dist/assets"
 	} else {
-		config.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/afa6add"
+		config.PatternLibraryAssetsPath = "//cdn.ons.gov.uk/dp-design-system/0ac6718"
 	}
 	return config, nil
 }
